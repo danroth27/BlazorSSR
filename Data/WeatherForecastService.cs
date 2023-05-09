7bb553638c81ab76089f0d09adc524ca6e4c94e2
Daniel Roth <daroth@microsoft.com>
2023-05-09 09:39:57 -0700

    Add setup for streaming rendering

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -10,13 +10,13 @@ public class WeatherForecastService
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+    public async Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
     {
-        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = startDate.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        }).ToArray());
+        }).ToArray();
     }
 }
